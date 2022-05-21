b8541b6acb936d37e5c7b666415dcf2dd5fbd560
khyw407 <khyw407@gmail.com>
2022-05-21 20:19:53 +0900

    feature: 파이어베이스 기반 로그인 기능 구현

---
+++ nwitter/src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import AppRouter from 'components/Router';
+import { authService } from 'fbase';
+
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn]  = useState(false);
+  
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user) {
+        setIsLoggedIn(user);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return (
+    <React.Fragment>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..."}
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </React.Fragment>
+  );
+}
+
+export default App;
